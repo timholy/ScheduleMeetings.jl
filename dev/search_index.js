e0044e221fa7d604aec71fabfb15d69c10a3e53a
Documenter.jl <documenter@juliadocs.github.io>
2023-03-13 20:47:32 +0000

    build based on 74a103a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScheduleMeetings","category":"page"},{"location":"#ScheduleMeetings","page":"Home","title":"ScheduleMeetings","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScheduleMeetings.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScheduleMeetings]","category":"page"}]
+}
