0301812f494acb90bfdcac18bd61aabcb14034ca
Tim Holy <tim.holy@gmail.com>
2023-10-30 16:50:15 -0500

    Switch output to DataFrame (#1)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,14 +4,21 @@ using Test
 
 @testset "ScheduleMeetings.jl" begin
     startlm, startjc = Date(2023, 3, 21), Date(2023, 3, 24)
-    targetslm = TargetEltype["A" => nothing, "B" => Target(Date(2023, 4, 18)), "C" => nothing]
-    targetsjc = TargetEltype["A" => nothing, "C" => nothing, "D" => nothing]
-    avoid = [Date(2023, 3, 26):Day(1):Date(2023, 3, 31)]
-    slm, sjc = schedule_meetings(targetslm, targetsjc, startlm, startjc; avoid)
-    @test slm[1] == ("A" => Date(2023, 3, 21))
-    @test slm[2] == ("B" => Date(2023, 4, 11))
-    @test slm[3] == ("C" => Date(2023, 4, 4))
-    @test sjc[1] == ("A" => Date(2023, 3, 24))
-    @test sjc[2] == ("C" => Date(2023, 4, 7))
-    @test sjc[3] == ("D" => Date(2023, 4, 14))
+    people = ["A", "B", "C"]
+    for (targetslm, targetsjc) in [
+        (TargetEltype["A" => nothing, "B" => Target(Date(2023, 4, 18)), "C" => nothing],
+         TargetEltype["A" => nothing, "C" => nothing, "D" => nothing]),
+        (TargetEltype["B" => Target(Date(2023, 4, 18))],
+        TargetEltype["A" => nothing, "C" => nothing, "D" => nothing])
+    ]
+        targetslm = complete_list(targetslm, people)
+        avoid = [Date(2023, 3, 26):Day(1):Date(2023, 3, 31)]
+        slm, sjc = schedule_meetings(targetslm, targetsjc, startlm, startjc; avoid)
+        @test slm[1, :Presenter] == "A" && slm[1, :Date] == Date(2023, 3, 21)
+        @test slm[2, :Presenter] == "C" && slm[2, :Date] == Date(2023, 4, 4)
+        @test slm[3, :Presenter] == "B" && slm[3, :Date] == Date(2023, 4, 11)
+        @test sjc[1, :Presenter] == "A" && sjc[1, :Date] == Date(2023, 3, 24)
+        @test sjc[2, :Presenter] == "C" && sjc[2, :Date] == Date(2023, 4, 7)
+        @test sjc[3, :Presenter] == "D" && sjc[3, :Date] == Date(2023, 4, 14)
+    end
 end
