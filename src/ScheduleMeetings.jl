b83364f67c785f3a00d32448354ef40379f9de10
Tim Holy <tim.holy@gmail.com>
2023-10-31 07:22:41 -0500

    Switch to exhaustive scheduling (#2)
    Evolutionary wasn't finding very good solutions.
    
    This also modifies the objective and consequently the tests

--- src/ScheduleMeetings.jl
+++ src/ScheduleMeetings.jl
@@ -3,7 +3,7 @@ module ScheduleMeetings
 using Dates
 using RDates
 using HolidayCalendars
-using Evolutionary
+using Combinatorics
 using DataFrames
 
 export Target, TargetEltype, schedule_meetings, complete_list
@@ -22,46 +22,36 @@ const DateRange = typeof(today():Day(1):today()+Day(5))
 penalty(_, ::Nothing) = Day(0)
 function penalty(date, target::Target)
     target.prior && date >= target.date && return Day(10^5)  # something big but won't overflow
-    return 3*(target.date - date)
+    return 10*(target.date - date)
 end
 
-function objective(dates, targets)
+function objective(assigned, targets, alldates)
     obj = Day(0)
-    prev = typemin(Date)
-    for (person, target) in targets
-        date = dates[person]
+    for ((person, target), ideal) in zip(targets, alldates)
+        date = assigned[person]
         # Try to hit target
         obj += penalty(date, target)
         # Penalize reordering
-        if date < prev
-            obj += prev - date
-        end
-        prev = date
+        obj += abs(date - ideal)
     end
     return obj
 end
 
-function penalize_proximity(dateslm, datesjc, people; minsep=Day(7))
+function penalize_proximity(assignedlm, assignedjc, people; minsep=Day(7))
     obj = Day(0)
     for person in people
-        datelm, datejc = get(dateslm, person, nothing), get(datesjc, person, nothing)
+        datelm, datejc = get(assignedlm, person, nothing), get(assignedjc, person, nothing)
         (datelm === nothing || datejc === nothing) && continue
         Δdate = abs(datelm - datejc)
         if Δdate < minsep
-            obj += minsep - Δdate
+            obj += 10*(minsep - Δdate)
         end
     end
     return obj
 end
 
-function objective_lm_jc(dateslm, datesjc, targetslm, targetsjc; kwargs...)
-    allpeople = Set{String}()
-    for itr in (targetslm, targetsjc)
-        for (person, _) in itr
-            push!(allpeople, person)
-        end
-    end
-    ret = objective(dateslm, targetslm) + objective(datesjc, targetsjc) + penalize_proximity(dateslm, datesjc, allpeople; kwargs...)
+function objective_lm_jc(assignedlm, assignedjc, targetslm, targetsjc, allpeople, dateslm, datesjc; kwargs...)
+    ret = objective(assignedlm, targetslm, dateslm) + objective(assignedjc, targetsjc, datesjc) + penalize_proximity(assignedlm, assignedjc, allpeople; kwargs...)
     return ret.value
 end
 
@@ -77,41 +67,77 @@ function filldates(start, n, calendars, avoid; gap=Day(7))
     return dates
 end
 
+function available(targets, dates)
+    people, taken = String[], Date[]
+    for (person, target) in targets
+        if target === nothing
+            push!(people, person)
+        else
+            push!(taken, target.date)
+        end
+    end
+    return people, setdiff(dates, taken)
+end
+
 function schedule_meetings(targetslm::typeof(default_target),
                            targetsjc::typeof(default_target),
                            startlm,
                            startjc;
                            calendars=ScheduleMeetings.calendars,
-                           avoid=DateRange[],
+                           avoidlm=nothing,
+                           avoidjc=nothing,
+                           avoid=nothing,
                            kwargs...)
+    if avoid !== nothing
+        @assert avoidlm === nothing && avoidjc === nothing
+        avoidlm = avoidjc = avoid
+    end
     nlm, njc = length(targetslm), length(targetsjc)
-    dateslm = filldates(startlm, nlm, calendars, avoid)
-    datesjc = filldates(startjc, njc, calendars, avoid)
+    dateslm = filldates(startlm, nlm, calendars, avoidlm)
+    datesjc = filldates(startjc, njc, calendars, avoidjc)
+    flexlm, availlm = available(targetslm, dateslm)
+    flexjc, availjc = available(targetsjc, datesjc)
+    allpeople = Set{String}()
+    for itr in (targetslm, targetsjc)
+        for (person, _) in itr
+            push!(allpeople, person)
+        end
+    end
 
-    builddict(dates, p, targets) = Dict(person => dates[i] for ((person, _), i) in zip(targets, p))
-    buildlist(dates, p, targets) = sort!([person => dates[i] for ((person, _), i) in zip(targets, p)]; by=last)
+    to_dict(pairs) = Dict{String,Date}(person => target.date for (person, target) in pairs if target !== nothing)
     to_df(pairs) = DataFrame("Presenter" => first.(pairs), "Date" => last.(pairs))
 
-    f(perm) = objective_lm_jc(builddict(dateslm, view(perm, 1:nlm), targetslm),
-                              builddict(datesjc, view(perm, nlm+1:nlm+njc), targetsjc),
-                              targetslm, targetsjc; kwargs...)
-    function swap2blocks(perm; rng=nothing)
-        if rand() < 0.5
-            swap2(view(perm, 1:nlm))
-        else
-            swap2(view(perm, nlm+1:nlm+njc))
+    tmplm, bestlm = to_dict(targetslm), to_dict(targetslm)
+    tmpjc, bestjc = to_dict(targetsjc), to_dict(targetsjc)
+    bestobj = typemax(Int)
+    for plm in permutations(availlm), pjc in permutations(availjc)
+        for (person, datelm) in zip(flexlm, plm)
+            tmplm[person] = datelm
         end
-        return perm
+        for (person, datejc) in zip(flexjc, pjc)
+            tmpjc[person] = datejc
+        end
+        val = objective_lm_jc(tmplm, tmpjc, targetslm, targetsjc, allpeople, dateslm, datesjc; kwargs...)
+        if val < bestobj
+            bestobj = val
+            copyto!(bestlm, tmplm)
+            copyto!(bestjc, tmpjc)
+        end
     end
-    perm0 = [1:nlm; 1:njc]
-    result = Evolutionary.optimize(f, perm0, GA(mutation=swap2blocks, ɛ=0.1), Evolutionary.Options(iterations=1000))
-    f(perm0) >= Evolutionary.minimum(result) || @warn "Initial permutation was better than final result"
-    perm = Evolutionary.minimizer(result)
-    return to_df(buildlist(dateslm, perm[1:nlm], targetslm)), to_df(buildlist(datesjc, perm[nlm+1:nlm+njc], targetsjc))
+
+    return to_df(sort!(collect(bestlm); by=last)), to_df(sort!(collect(bestjc); by=last))
 end
 
 complete_list(targeted::AbstractDict, all) = TargetEltype[k => get(targeted, k, nothing) for k in all]
 complete_list(targeted, all) = complete_list(Dict(k => v for (k, v) in targeted), all)
+
+# private copyto!
+function copyto!(dest, src)
+    for (k, v) in src
+        dest[k] = v
+    end
+    return dest
+end
 
 function __init__()
     push!(calendars, calendar(CALENDARS, "US/SETTLEMENT"))
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,19 +6,19 @@ using Test
     startlm, startjc = Date(2023, 3, 21), Date(2023, 3, 24)
     people = ["A", "B", "C"]
     for (targetslm, targetsjc) in [
-        (TargetEltype["A" => nothing, "B" => Target(Date(2023, 4, 18)), "C" => nothing],
+        (TargetEltype["A" => nothing, "B" => Target(Date(2023, 4, 11)), "C" => nothing],
          TargetEltype["A" => nothing, "C" => nothing, "D" => nothing]),
-        (TargetEltype["B" => Target(Date(2023, 4, 18))],
+        (TargetEltype["B" => Target(Date(2023, 4, 11))],
         TargetEltype["A" => nothing, "C" => nothing, "D" => nothing])
     ]
-        targetslm = complete_list(targetslm, people)
+        targetslm = sort!(complete_list(targetslm, people); by=first)
         avoid = [Date(2023, 3, 26):Day(1):Date(2023, 3, 31)]
         slm, sjc = schedule_meetings(targetslm, targetsjc, startlm, startjc; avoid)
         @test slm[1, :Presenter] == "A" && slm[1, :Date] == Date(2023, 3, 21)
         @test slm[2, :Presenter] == "C" && slm[2, :Date] == Date(2023, 4, 4)
         @test slm[3, :Presenter] == "B" && slm[3, :Date] == Date(2023, 4, 11)
-        @test sjc[1, :Presenter] == "A" && sjc[1, :Date] == Date(2023, 3, 24)
-        @test sjc[2, :Presenter] == "C" && sjc[2, :Date] == Date(2023, 4, 7)
+        @test sjc[1, :Presenter] == "C" && sjc[1, :Date] == Date(2023, 3, 24)
+        @test sjc[2, :Presenter] == "A" && sjc[2, :Date] == Date(2023, 4, 7)
         @test sjc[3, :Presenter] == "D" && sjc[3, :Date] == Date(2023, 4, 14)
     end
 end
